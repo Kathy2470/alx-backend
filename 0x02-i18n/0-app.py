b9c512b9688db3a512617c37cbe3c0d77748ab7b
kathy2470 <mukisakathy@gmail.com>
2024-07-30 17:08:32 +0300

    Task 0 update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,9 +3,12 @@
 Flask app for basic setup.
 """
 
+
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -13,6 +16,7 @@ def index():
     Route for the index page.
     """
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
